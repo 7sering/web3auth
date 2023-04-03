6fc35bf9097b095f61feb3cd4e705e490cf1eadd
coder <codenow8@gmail.com>
2023-04-03 20:46:56 +0545

    Auth all function worked successfully

--- src/pages/Welcome.jsx
+++ src/pages/Welcome.jsx
@@ -1,11 +1,32 @@
-import React from 'react'
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { connectWallet } from "../components/web3Connect";
 
 const Welcome = () => {
-  return (
-   <>
-    <h2>Welcome To Marketplace</h2>
-   </>
-  )
-}
+  const navigate = useNavigate();
 
-export default Welcome
+  useEffect(() => {
+    const checkAuth = setInterval(() => {
+      verifyUser();
+    }, 2000);
+    return () => clearInterval(checkAuth);
+  },[verifyUser]);
+
+  async function verifyUser() {
+    const output = await connectWallet();
+    console.log(output);
+    if (output === 0) {
+      navigate("/denied");
+    } else {
+      navigate("/welcome");
+    }
+  }
+
+  return (
+    <>
+      <h2>Welcome To Marketplace</h2>
+    </>
+  );
+};
+
+export default Welcome;
