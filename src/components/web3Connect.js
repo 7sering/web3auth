e5b1b3f22683e289d5254b4ce23b8d69ae226e2e
coder <codenow8@gmail.com>
2023-04-03 17:17:50 +0545

    Auth is worked on  Welcome/denied page

--- src/components/web3Connect.js
+++ src/components/web3Connect.js
@@ -3,14 +3,19 @@ import NFTAbi from "./nftAbi.json";
 import { ethers } from "ethers";
 import Web3Modal from "web3modal";
 
-export async function connectWallet(){
-    const web3modal = new Web3Modal()
-    const connection = await web3modal.connect()
-    const provider = new ethers.providers.Web3Provider(connection)
-    const signer = provider.getSigner()
-    const addressraw = signer.getAddress()
-    const addressstr = (await addressraw).valueOf()
-    let contract = new ethers.Contract(nftContract, NFTAbi, signer)
-    let getIds = await contract.walletOfOwner(addressstr)
-    console.log(getIds[0])
+export async function connectWallet() {
+  const web3modal = new Web3Modal();
+  const connection = await web3modal.connect();
+  const provider = new ethers.providers.Web3Provider(connection);
+  const signer = provider.getSigner();
+  const addressraw = signer.getAddress();
+  const addressstr = (await addressraw).valueOf();
+  let contract = new ethers.Contract(nftContract, NFTAbi, signer);
+  let getIds = await contract.walletOfOwner(addressstr);
+  // console.log(getIds[0])
+  if (getIds[0] === undefined) {
+    return 0;
+  } else {
+    return 1;
+  }
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,29 @@
-import React from 'react'
-import {connectWallet} from "../components/web3Connect"
+import React from "react";
+import { connectWallet } from "../components/web3Connect";
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
+  const navigate = useNavigate();
+  async function verifyUser() {
+    const output = await connectWallet();
+    console.log(output);
+    if (output === 0) {
+      navigate("/denied");
+    } else {
+      navigate("/welcome");
+    }
+  }
   return (
-  <>
-    <h2>Market Place Home</h2>
-    <button style={{background: "green", color: "white"}} onClick={connectWallet}>Connect Wallet</button>
-  </>
-  )
-}
+    <>
+      <h2>Market Place Home</h2>
+      <button
+        style={{ background: "green", color: "white" }}
+        onClick={verifyUser}
+      >
+        Connect Wallet
+      </button>
+    </>
+  );
+};
 
-export default Home
+export default Home;
